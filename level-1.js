0c6d2c407b11e8a35d8e46aa569941099301a1bc
torandre94 <61887464+torandre94@users.noreply.github.com>
2020-08-19 08:24:36 +0200

    Update level-1.js

--- level-1.js
+++ level-1.js
@@ -1,1 +1,38 @@
+// question 1
+var motorcycle = "Yamaha";
 
+// question 2
+var person = {
+  name: "Ola Normann",
+  age: "25"
+};
+
+// question 3
+var outOfStock = true;
+
+if(outOfStock === true) {
+  console.log("Out of stock");
+}
+else {
+  console.log("In stock");
+}
+
+// question 4
+
+
+// question 5
+
+
+// question 6
+
+
+// question 7
+
+
+// question 8
+
+
+// question 9
+
+
+// question 10
